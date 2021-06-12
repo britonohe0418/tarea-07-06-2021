89cff8ce8f1ba71e116d3bda13fc934b286ac9a7
britonohe0418 <anonymous>
2021-06-12 00:03:03 -0500

    ej2 creacion de variables edad/peso.

--- tarea.py
+++ tarea.py
@@ -1,12 +1,19 @@
-num = 65
-if type(num) == int:
-    print("Resultado:", num * 2)
-else:
-    print("Este dato NO es numérico.")
+# num = 65
+# if type(num) == int:
+#     print("Resultado:", num * 2)
+# else:
+#     print("Este dato NO es numérico.")
 
-def sms(men):
-    print(men)
+# def sms(men):
+#     print(men)
 
 
-sms("Programa #1.")
-sms("Programa #2.")
+# sms("Programa #1.")
+# sms("Programa #2.")
+
+
+class Sintax:
+    def usoVariable(self):
+        edad, _peso = 70, 51.62
+        print("Edad de la persona: {}".format(edad))
+        print("Peso de la persona: {}".format(_peso))
